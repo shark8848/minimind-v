f66cd0f26d10468039afff9a4d1ae871b94a3e67
shark8848 <46478158+shark8848@users.noreply.github.com>
2025-04-16 10:05:10 +0800

    Create train_pretrain_vlm.sh

---
+++ train_pretrain_vlm.sh
@@ -0,0 +1,1 @@
+python train_pretrain_vlm.py   --data_path "./dataset/minimind-v_dataset/pretrain_data.jsonl"   --images_path "./dataset/minimind-v_dataset/pretrain_images"   --dim 768   --epochs 4 --use_wandb
